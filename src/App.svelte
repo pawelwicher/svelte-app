006e2765ab874392c4a30bb061cfc6d7e3f553b4
Paweł Wicher <pawel.wicher@pse.pl>
2021-12-25 13:43:29 +0100

    svelte app init

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Router from 'svelte-spa-router';
+  import Home from './pages/Home.svelte';
+  import About from './pages/About.svelte';
+
+  type App = { title: string }
+
+  let app: App = { title: 'My Svelte App' };
+
+</script>
+
+<nav>
+  <a href="/#/">Home</a>
+  <a href="/#/about">About</a>
+</nav>
+
+<main>
+  <h1>{ app.title }</h1>
+
+  <Router routes={{
+    '/': Home,
+    '/about': About
+  }} />
+    
+</main>
+
+<style>
+  h1 {
+    color: cornflowerblue;
+  }
+</style>
---
+++ src/pages/About.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  
+  let promise = fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon")
+    .then(response => response.json());
+    
+</script>
+
+{#await promise}
+  <p>...waiting</p>
+{:then data}
+  <ul>
+    {#each data.drinks as d}
+      <li>{d.strDrink}</li>
+    {/each}
+  </ul>
+{:catch error}
+  <p>{error.message}</p>
+{/await}
